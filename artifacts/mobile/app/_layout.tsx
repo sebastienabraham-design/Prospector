0c738a99f2e54d80cbeed9a6333aae2b7699967c
sebastienabraham-design <sebastien.abraham@safti.fr>
2026-04-21 00:12:54 +0200

    Debug: remove KeyboardProvider to isolate boot crash
    react-native-keyboard-controller is a native third-party module that may
    not be bundled in Expo Go SDK 54. Removing temporarily to see if the
    boot crash goes away. Will re-add behind a dev-build check once confirmed.

--- artifacts/mobile/app/_layout.tsx
+++ artifacts/mobile/app/_layout.tsx
@@ -10,7 +10,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { setBaseUrl } from "@workspace/api-client-react";
 
@@ -105,10 +104,8 @@ export default function RootLayout() {
         <QueryClientProvider client={queryClient}>
           <SyncProvider>
             <GestureHandlerRootView>
-              <KeyboardProvider>
-                <SyncStatusBar />
-                <RootLayoutNav />
-              </KeyboardProvider>
+              <SyncStatusBar />
+              <RootLayoutNav />
             </GestureHandlerRootView>
           </SyncProvider>
         </QueryClientProvider>
